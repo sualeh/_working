10e52e8511470ee01a502b07f630108330936321
Sualeh Fatehi <sualeh@hotmail.com>
2014-08-05 20:28:42 -0400

    Initial commit

---
+++ src/main/java/us/fatehi/working/RegularExpressionMultipleExcludes.java
@@ -0,0 +1,28 @@
+package us.fatehi.working;
+
+
+import java.util.regex.Pattern;
+
+public class RegularExpressionMultipleExcludes
+{
+
+  private static String regex = ".*\\.(?!(MDRS_|MDRT_|MDOT_)).*\\..*";
+  //                             .*\.(?!(MDRS_|MDRT_|MDOT_)).*
+
+  public static boolean excludeMultiple(String text)
+  {
+    final boolean matches = Pattern.matches(regex, text);
+    System.out.println(text + "=" + matches);
+    return matches;
+  }
+
+  public static void main(String[] args)
+  {
+    System.out.println(regex);
+    excludeMultiple("XX.MDRS_YY.ZZZZ");
+    excludeMultiple("XX.MDRT_YY.ZZZZ");
+    excludeMultiple("XX.MDOT_YY.ZZZZ");
+    excludeMultiple("XX.VAP_YY.ZZZZ");
+  }
+
+}
