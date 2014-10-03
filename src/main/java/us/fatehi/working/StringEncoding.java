b95d06671860fc59a861c6418a5630fbf9731334
Sualeh Fatehi <sualeh@hotmail.com>
2014-10-02 23:05:30 -0400

    Run through all character sets

--- src/main/java/us/fatehi/working/StringEncoding.java
+++ src/main/java/us/fatehi/working/StringEncoding.java
@@ -9,84 +9,137 @@ import java.nio.charset.Charset;
 public class StringEncoding
 {
 
-  private static final boolean DEBUG = false;
-
   public static void main(final String[] args)
     throws Exception
   {
-    printAllCharsets();
-    final Charset charset = Charset.forName("ISO-8859-1");
-    System.out.println("Using charset: " + charset);
 
-    final byte[] b = new byte[1024];
-    for (int i = 0; i < 4; i++)
+    final byte[] b = _build();
+
+    for (final Charset charset: Charset.availableCharsets().values())
     {
-      for (int j = 0; j < 256; j++)
-      {
-        b[i * 256 + j] = (byte) j;
-      }
+      _test(charset, b);
     }
-
-    printByteArray(b);
-
-    final String s = new String(b, charset);
-    final byte[] c = s.getBytes(charset);
-    compareByteArrays(b, c);
-
-    printByteArray(c);
 
   }
 
-  private static void compareByteArrays(final byte[] a1, final byte[] a2)
+  private static byte[] _build()
+  {
+    final byte[] b = new byte[2048];
+    int index = 0;
+    for (int i = 0; i < 4; i++)
+    {
+      for (int j = 0; j < 512; j++)
+      {
+        index = i * 256 + j;
+        b[index] = (byte) j;
+        // System.out.println(i + "=" + b[index]);
+      }
+    }
+    for (byte j = Byte.MIN_VALUE; j <= 0; j++)
+    {
+      b[index] = j;
+      // System.out.println(i + "=" + b[index]);
+      index++;
+
+      b[index] = (byte) (-((int) j) - 1);
+      // System.out.println(i + "=" + b[index]);
+      index++;
+    }
+    for (byte j = Byte.MAX_VALUE; j >= 0; j--)
+    {
+      b[index] = j;
+      // System.out.println(index + "=" + b[index]);
+      index++;
+
+      b[index] = (byte) (-((int) j) - 1);
+      // System.out.println(index + "=" + b[index]);
+      index++;
+    }
+    return b;
+  }
+
+  private static void _test(final Charset charset, final byte[] b)
+  {
+    try
+    {
+
+      printByteArray(b);
+
+      final String s = new String(b, charset);
+      final byte[] c = s.getBytes(charset);
+      final int i = compareByteArrays(b, c);
+      switch (i)
+      {
+        case 0:
+          System.out.print("-->> Match <<--                                ");
+          System.out.println(charset);
+          break;
+        case -1:
+          if (MESSAGE)
+          {
+            System.out.print("Different lengths                              ");
+            System.out.println(charset);
+          }
+          break;
+        default:
+          if (MESSAGE)
+          {
+            System.out.print(String.format("Location %03d: ", i));
+            printByte(b[i]);
+            System.out.print(" != ");
+            printByte(c[i]);
+            System.out.print("   ");
+            System.out.println(charset);
+          }
+          break;
+      }
+
+      printByteArray(c);
+    }
+    catch (final Exception e)
+    {
+      if (MESSAGE)
+      {
+        System.out.print("Encoding error                                 ");
+        System.out.println(charset);
+      }
+    }
+  }
+
+  private static int compareByteArrays(final byte[] a1, final byte[] a2)
   {
     if (a1 == a2)
     {
-      return;
+      return 0;
     }
     if (a1 == null || a2 == null)
     {
-      return;
+      return 0;
     }
 
     final int length = a1.length;
     if (a2.length != length)
     {
-      return;
+      return -1;
     }
 
     for (int i = 0; i < length; i++)
     {
       if (a1[i] != a2[i])
       {
-        System.out.print("Array contents do not match at location " + i + ": ");
-        printByte(a1[i]);
-        System.out.print(" != ");
-        printByte(a2[i]);
-        return;
+        return i;
       }
     }
 
-    System.out.println("Array contents are the same");
-    return;
-  }
-
-  private static void printAllCharsets()
-  {
-    if (!DEBUG)
-    {
-      return;
-    }
-    System.out.println("All charsets:\n");
-    System.out.println(Charset.availableCharsets().keySet().toString()
-      .replaceAll("\\[", "").replaceAll(", ", "\n").replaceAll("\\]", ""));
+    return 0;
   }
 
   private static void printByte(final byte b)
   {
-    System.out.print(String.format("%03d=%s",
-                                   (int) b,
-                                   Integer.toBinaryString(b & 255 | 256)
-                                     .substring(1)));
+    final int intB = b;
+    System.out.print((intB < 0? "-": " ")
+                     + String.format("%03d=%s", Math.abs(intB), Integer
+                       .toBinaryString(b & 255 | 256).substring(1)));
   }
 
   private static void printByteArray(final byte[] b)
@@ -111,5 +164,8 @@ public class StringEncoding
       }
     }
   }
+
+  private static final boolean MESSAGE = true;
+  private static final boolean DEBUG = false;
 
 }
