42139c940cbc1300cab9950a1fe7655931da5232
Sualeh Fatehi <sualeh@hotmail.com>
2014-10-06 14:08:30 -0400

    Decode

--- src/main/java/us/fatehi/working/Base64Decode.java
+++ src/main/java/us/fatehi/working/Base64Decode.java
@@ -1,6 +1,7 @@
 package us.fatehi.working;
 
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 import org.apache.commons.codec.binary.Base64;
@@ -19,26 +20,12 @@ public class Base64Decode
         final String base64Encoded = in.next();
         final byte[] decodeBase64 = Base64.decodeBase64(base64Encoded);
         System.out.println("Base-64 encoded string: " + base64Encoded);
-        System.out.println(String.format("Decoded to %d binary bytes",
+        System.out.println(String.format("Decoded to %d bytes",
                                          decodeBase64.length));
-        System.out.println("Decoded byes:");
-        for (int i = 0; i < decodeBase64.length; i++)
-        {
-          final byte b = decodeBase64[i];
-          System.out.print(String.format("Byte #%03d: ", i));
-          printByte(b);
-          System.out.println();
-        }
+        System.out.println(String.format("Hex encoded: %040x",
+                                         new BigInteger(1, decodeBase64)));
       }
     }
-  }
-
-  private static void printByte(final byte b)
-  {
-    final int intB = b;
-    System.out.print((intB < 0? "-": " ")
-                     + String.format("%03d=%s", Math.abs(intB), Integer
-                       .toBinaryString(b & 255 | 256).substring(1)));
   }
 
 }
