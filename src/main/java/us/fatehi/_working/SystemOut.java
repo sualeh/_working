f0c4efe6b0470043377309615bb044a5cf2afa87
Sualeh Fatehi <sualeh@hotmail.com>
2015-11-18 13:11:46 -0500

    More tests

---
+++ src/main/java/us/fatehi/_working/SystemOut.java
@@ -0,0 +1,15 @@
+package us.fatehi._working;
+
+
+public class SystemOut
+{
+
+  public static void main(String[] args)
+    throws Exception
+  {
+    System.out.println("Print one");
+    System.out.close();
+    System.out.println("Print two");
+  }
+
+}
