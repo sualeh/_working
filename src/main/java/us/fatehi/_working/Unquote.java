38130834bc9626930d093e60b2c19df1f2e82e8c
Sualeh Fatehi <sualeh@hotmail.com>
2016-02-14 17:38:58 -0500

    Unquote

---
+++ src/main/java/us/fatehi/_working/Unquote.java
@@ -0,0 +1,40 @@
+package us.fatehi._working;
+
+
+import java.util.regex.Pattern;
+
+public class Unquote
+{
+  public static void main(String[] args)
+  {
+
+  }
+
+  protected static String unquotedName(final String name)
+  {
+    if (isBlank(name) && name.length() < 3)
+    {
+      return name;
+    }
+
+    String unquotedName = name;
+
+    if (Pattern.matches("\\p{Punct}.*\\p{Punct}", name))
+    {
+      final char firstChar = name.charAt(0);
+      final char lastChar = name.charAt(name.length() - 1);
+      if (firstChar == lastChar)
+      {
+        unquotedName = name.substring(1, name.length() - 1);
+      }
+    }
+    return unquotedName;
+
+  }
+
+  private static boolean isBlank(String name)
+  {
+    return name == null || name.trim().isEmpty();
+  }
+
+}
