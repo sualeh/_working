9e4222640635b8d924d17e1d98ca3aff4a443d68
Sualeh Fatehi <sualeh@hotmail.com>
2016-03-29 23:16:54 -0400

    Updates

---
+++ src/main/java/us/fatehi/_working/Uppercase.java
@@ -0,0 +1,14 @@
+package us.fatehi._working;
+
+
+public class Uppercase
+{
+
+  public static void main(String[] args)
+  {
+    final String greek = "σκύλος";
+    final String upperCase = greek.toUpperCase();
+    System.out.println(upperCase);
+  }
+
+}
