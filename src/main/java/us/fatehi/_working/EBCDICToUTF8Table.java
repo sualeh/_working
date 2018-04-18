cecdbf54278adbb2577dadbd37f0df0fa44c7cca
Sualeh Fatehi <sualeh@hotmail.com>
2018-04-18 00:06:57 -0400

    Create a normalization method

--- src/main/java/us/fatehi/_working/EBCDICToUTF8Table.java
+++ src/main/java/us/fatehi/_working/EBCDICToUTF8Table.java
@@ -3,6 +3,7 @@ package us.fatehi._working;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.text.Normalizer;
 
 public class EBCDICToUTF8Table
 {
@@ -17,16 +18,22 @@ public class EBCDICToUTF8Table
       for (int i = 0; i < 256; i++)
       {
         // Convert EBCDIC character to a Java string
-        final String chstr = new String(new byte[] { (byte) i }, charset);
+        final byte ebcdicChar = (byte) i;
+        final String chstr = new String(new byte[] { ebcdicChar }, charset);
+        final char normchstr = normalizeChar(ebcdicChar, charset);
+
         // Get character (code point) properties
         final int ch = chstr.codePointAt(0);
         final int numUTF8Bytes = chstr.getBytes(StandardCharsets.UTF_8).length;
         final String name = Character.getName(ch);
-        final boolean isISOControl = Character.isISOControl(ch);
+        final boolean isISOControl = Character
+          .isISOControl(chstr.codePointAt(0));
         final boolean isASCIIPrintable = ch > 31 && ch < 127;
+
         // Sanity checks
         final boolean alternateASCIIPrintable = numUTF8Bytes == 1
                                                 && !isISOControl;
+
         if (isASCIIPrintable && !alternateASCIIPrintable)
         {
           throw new Exception(String
@@ -35,10 +42,12 @@ public class EBCDICToUTF8Table
                     numUTF8Bytes,
                     isISOControl));
         }
+
         // Print properties
         System.out.println(String
-          .format("\"%s\"; %s printable; EBCDIC code point %3d; Unicode code point %3d; code page %s; Unicode name %s;",
+          .format("original \"%s\"; converted \"%s\"; %s ASCII printable; EBCDIC code point %3d; Unicode code point %3d; code page %s; Unicode name %s;",
                   isISOControl? "?": chstr,
+                  normchstr,
                   isASCIIPrintable? "   ": "not",
                   i,
                   ch,
@@ -46,6 +55,26 @@ public class EBCDICToUTF8Table
                   name));
       }
       System.out.println("\n\n");
+    }
+  }
+
+  private static char normalizeChar(final byte ebcdicChar,
+                                    final Charset charset)
+  {
+    // Decode the EBCDIC character (which is a byte), based on the
+    // EBCDIC charset
+    final String decodedString = new String(new byte[] { ebcdicChar }, charset);
+    // Normalize by removing accents and diacriticals
+    final char normalizedChar = Normalizer
+      .normalize(decodedString, Normalizer.Form.NFKD).charAt(0);
+    // Check for characters within the ASCII printable range
+    if (normalizedChar > 31 && normalizedChar < 127)
+    {
+      return normalizedChar;
+    }
+    else
+    {
+      return '?';
     }
   }
 
