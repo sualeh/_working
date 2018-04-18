a3fbcbd0d635277ad1d6b6f24ab49201811fd046
Sualeh Fatehi <sualeh@hotmail.com>
2018-04-17 23:27:02 -0400

    More organized

--- src/main/java/us/fatehi/_working/EBCDICToUTF8Table.java
+++ src/main/java/us/fatehi/_working/EBCDICToUTF8Table.java
@@ -8,6 +8,7 @@ public class EBCDICToUTF8Table
 {
 
   public static void main(final String[] args)
+    throws Exception
   {
     final String[] EBCDIC_CODE_PAGES = { "IBM037", "IBM01148", "IBM1047" };
     for (final String element: EBCDIC_CODE_PAGES)
@@ -22,14 +23,27 @@ public class EBCDICToUTF8Table
         final int numUTF8Bytes = chstr.getBytes(StandardCharsets.UTF_8).length;
         final String name = Character.getName(ch);
         final boolean isISOControl = Character.isISOControl(ch);
-        final boolean isASCIIPrintable = numUTF8Bytes == 1 && !isISOControl;
+        final boolean isASCIIPrintable = ch > 31 && ch < 127;
+        // Sanity checks
+        final boolean alternateASCIIPrintable = numUTF8Bytes == 1
+                                                && !isISOControl;
+        if (isASCIIPrintable && !alternateASCIIPrintable)
+        {
+          throw new Exception(String
+            .format("%3d %d %b - We have not detected printable ASCII characters correctly",
+                    ch,
+                    numUTF8Bytes,
+                    isISOControl));
+        }
         // Print properties
-        System.out.println(String.format("%s %3d = %s %s = %s",
-                                         charset.displayName(),
-                                         i,
-                                         isISOControl? "?": chstr,
-                                         !isASCIIPrintable? "?": chstr,
-                                         name));
+        System.out.println(String
+          .format("\"%s\"; %s printable; EBCDIC code point %3d; Unicode code point %3d; code page %s; Unicode name %s;",
+                  isISOControl? "?": chstr,
+                  isASCIIPrintable? "   ": "not",
+                  i,
+                  ch,
+                  charset.displayName(),
+                  name));
       }
       System.out.println("\n\n");
     }
