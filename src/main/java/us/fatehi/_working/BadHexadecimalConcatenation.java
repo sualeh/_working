fc7b28f968dcdd3c93b779e0eed42da6ecbc0a4c
Sualeh Fatehi <sualeh@hotmail.com>
2018-06-13 11:00:07 -0400

    Bad hexadecimal concatenation

---
+++ src/main/java/us/fatehi/_working/BadHexadecimalConcatenation.java
@@ -0,0 +1,42 @@
+package us.fatehi._working;
+
+/**
+ * <a href="https://find-sec-bugs.github.io/bugs.htm#BAD_HEXA_CONVERSION">Bad hexadecimal concatenation</a>
+ *
+ */
+public class BadHexadecimalConcatenation
+{
+  
+  public static void main(final String[] args)
+    throws Exception
+  {
+    final byte[] bytes1 = new byte[] {0x06, 0x79};
+    System.out.println(buildString1(bytes1));
+    System.out.println(buildString2(bytes1));
+
+    final byte[] bytes2 = new byte[] {0x67, 0x09};
+    System.out.println(buildString1(bytes2));
+    System.out.println(buildString2(bytes2));
+  }
+
+  private static String buildString1(final byte[] bytes)
+  {
+    final StringBuilder stringBuilder = new StringBuilder();
+    for (final byte b: bytes)
+    {
+      stringBuilder.append(Integer.toHexString(b & 0xFF));
+    }
+    return stringBuilder.toString();
+  }
+
+  private static String buildString2(final byte[] bytes)
+  {
+    final StringBuilder stringBuilder = new StringBuilder();
+    for (final byte b: bytes)
+    {
+      stringBuilder.append(String.format("%02X", b));
+    }
+    return stringBuilder.toString();
+  }
+  
+}
