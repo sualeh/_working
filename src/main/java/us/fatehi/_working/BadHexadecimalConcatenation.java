c36886d1b18c4c17a16c2235c9c5941a84556916
Sualeh Fatehi <sualeh@hotmail.com>
2018-06-14 17:19:55 -0400

    More tests

--- src/main/java/us/fatehi/_working/BadHexadecimalConcatenation.java
+++ src/main/java/us/fatehi/_working/BadHexadecimalConcatenation.java
@@ -1,22 +1,33 @@
 package us.fatehi._working;
 
+
 /**
- * <a href="https://find-sec-bugs.github.io/bugs.htm#BAD_HEXA_CONVERSION">Bad hexadecimal concatenation</a>
- *
+ * <a href=
+ * "https://find-sec-bugs.github.io/bugs.htm#BAD_HEXA_CONVERSION">Bad
+ * hexadecimal concatenation</a>
  */
 public class BadHexadecimalConcatenation
 {
-  
+
   public static void main(final String[] args)
     throws Exception
   {
-    final byte[] bytes1 = new byte[] {0x06, 0x79};
+    final byte[] bytes1 = new byte[] {
+                                       0x06,
+                                       0x79
+    };
     System.out.println(buildString1(bytes1));
     System.out.println(buildString2(bytes1));
 
-    final byte[] bytes2 = new byte[] {0x67, 0x09};
+    final byte[] bytes2 = new byte[] {
+                                       0x67,
+                                       0x09
+    };
     System.out.println(buildString1(bytes2));
     System.out.println(buildString2(bytes2));
+
+    System.out.println(Integer.toHexString(0x6709));
+    System.out.println(Integer.toHexString(0x0679));
   }
 
   private static String buildString1(final byte[] bytes)
@@ -38,5 +49,5 @@ public class BadHexadecimalConcatenation
     }
     return stringBuilder.toString();
   }
-  
+
 }
