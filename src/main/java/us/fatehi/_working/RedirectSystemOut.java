13487c73fa1229c74a9791e0a634b791a41e2b47
sualeh <sualeh@hotmail.com>
2018-10-17 21:31:56 -0400

    Redirect System.out

---
+++ src/main/java/us/fatehi/_working/RedirectSystemOut.java
@@ -0,0 +1,32 @@
+package us.fatehi._working;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+public class RedirectSystemOut
+{
+
+  // https://stackoverflow.com/questions/5339499/resetting-standard-output-stream
+  public static void main(String[] args)
+  {
+    // Print sample
+    System.out.println("Test - Before");
+
+    // Redirect System.out
+    final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outContent));
+    System.out.print("Test - Output to stream");
+    String printed = outContent.toString();
+    if (!"Test - Output to stream".equals(printed)) {
+      throw new RuntimeException("\"" + printed + "\"");
+    }
+    
+    // Reset System.out
+    System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+    System.out.println("Test - After");
+  }
+
+}
